bf6e238fecc0a6a56cda3970de05d0f6f61bfffe
Sudipta Sahu <sudipta8534@gmail.com>
2025-01-24 20:32:49 +0530

    Initial commit

---
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './ExploreMenu.css'
+import { menu_list } from '../../assets/assets'
+
+const ExploreMenu = ({category,setCategory}) => {
+  return (
+    <div className='explore-menu' id='explore-menu'>
+        <h1>Explore our menu</h1>
+        <p className='explore-menu-text'>Feeling hungry? Choose from a vast array of restaurants and cuisines to beat your hunger pangs.</p>
+        <div className="explore-menu-list">
+            {menu_list.map((item,index)=>{
+                return (
+                    <div onclick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className="explore-menu-list-item">
+                        <img className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
+                        <p>{item.menu_name}</p>
+                    </div>
+                )
+            })}
+        </div>
+        <hr />
+    </div>
+  )
+}
+
+export default ExploreMenu
