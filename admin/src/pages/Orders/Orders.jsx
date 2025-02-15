761f6964e469184daf887f1f307c217ec995c4ec
Sudipta Sahu <sudipta8534@gmail.com>
2025-02-15 23:42:03 +0530

    15th Feb

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './Orders.css'
-const Orders = () => {
+import { useState } from 'react'
+
+const Orders = ({url}) => {
+
+  const [orders,setOrders]=useState([]);
+
+  const fetchAllOrders=async()=>{
+    const response=await axios.get()
+  }
+
   return (
     <div>
-      Orders
+      
     </div>
   )
 }
