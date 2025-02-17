99457d7f1d546d1d0a90a9133ef558a9d1b38f86
Sudipta Sahu <sudipta8534@gmail.com>
2025-02-17 18:08:28 +0530

    17th Feb

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -20,6 +20,16 @@ const Orders = ({url}) => {
     }
   }
 
+  const statusHandler=async(event,orderId)=>{
+    const response=await axios.post(url+"/api/order/status",{
+      orderId,
+      status:event.target.value 
+    })
+    if (response.data.success) {
+      await fetchAllOrders();
+    }
+  }
+
   useEffect(()=>{
     fetchAllOrders();
   },[])
@@ -40,7 +50,20 @@ const Orders = ({url}) => {
                     </span> 
                   ))}
                 </p>
+                <p className='order-item-name'>{order.address.firstName+" "+order.address.lastName}</p>
+                <div className="order-item-address">
+                  <p>{order.address.street+","}</p>
+                  <p>{order.address.city+", "+order.address.state+", "+order.address.country+", "+order.address.zipcode}</p>
+                </div>
+                <p className='order-item-phone'>{order.address.phone}</p>
               </div>
+              <p>Items : {order.items.length}</p>
+              <p>${order.amount}</p>
+              <select onChange={(event)=>statusHandler(event,order._id)} value={order.status}> 
+                <option value="Food is being prepared">Food is being prepared</option>
+                <option value="Out For Delivery">Out For Delivery</option>
+                <option value="Delivered">Delivered</option>
+              </select>
             </div>
           ); 
         })}
