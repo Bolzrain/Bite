0a4d9722da69d28b75698a3b8f68857497cd413e
Sudipta Sahu <sudipta8534@gmail.com>
2025-03-24 20:27:50 +0530

    Your commit message

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,38 @@
-import express from "express"
-import cors from "cors"
-import { connectDB } from "./config/db.js"
-import foodRouter from "./routes/foodRoute.js"
-import userRouter from "./routes/userRoute.js"
-import 'dotenv/config'
-import cartRouter from "./routes/cartRoute.js"
-import orderRouter from "./routes/orderRoute.js"
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import "dotenv/config";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+import path from "path";
 
 //app config
-const app=express()
-const port= process.env.PORT ||4000;
+const app = express();
+const port = process.env.PORT || 4000;
+const dirname = path.resolve();
 
 //middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 //db connection
 connectDB();
 
 //api endpoints
-app.use("/api/food",foodRouter)
-app.use("/images",express.static('uploads'))
-app.use("/api/user",userRouter)
-app.use("/api/cart",cartRouter)
-app.use("/api/order",orderRouter)
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
+app.use(express.static(path.join(dirname, "/frontend/dist")));
 
-app.get("/",(req,res)=>{
-    res.send("API Working")
-})
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
 
-app.listen(port,()=>{
-    console.log(`Server started on http://localhost:${port}`)
-})
-
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='header'>
       <div className="header-contents">
-        <h2>Order your favorite food here.</h2>
+        <h2>Hungry? Let's fix that!</h2>
         <p>Feeling hungry? Choose from a vast array of restaurants and cuisines to beat your hunger pangs.</p>
         <button>View Menu</button>
       </div>
